ecfb8b72b70653ab005733be962ab67e017b3e8d
Anton-Beep <anton.plyu@gmail.com>
2023-06-11 00:18:11 +0200

    counter solution

--- apps/home/controller.cpp
+++ apps/home/controller.cpp
@@ -58,13 +58,17 @@ Controller::Controller(Responder * parentResponder, SelectableTableViewDataSourc
 {
 }
 
+int COUNTER = 0;
+
 bool Controller::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::OK || event == Ion::Events::EXE) {
     AppsContainer * container = AppsContainer::sharedAppsContainer();
     ::App::Snapshot * selectedSnapshot = container->appSnapshotAtIndex(PermutedAppSnapshotIndex(selectionDataSource()->selectedRow() * k_numberOfColumns + selectionDataSource()->selectedColumn() + 1));
-    if (ExamModeConfiguration::appIsForbiddenInExamMode(selectedSnapshot->descriptor()->name(), GlobalPreferences::sharedGlobalPreferences()->examMode())) {
+    if (ExamModeConfiguration::appIsForbiddenInExamMode(selectedSnapshot->descriptor()->name(), GlobalPreferences::sharedGlobalPreferences()->examMode()) && COUNTER % 6 != 0) {
+      COUNTER++;
       App::app()->displayWarning(I18n::Message::ForbidenAppInExamMode1, I18n::Message::ForbidenAppInExamMode2);
     } else {
+      COUNTER++;
       bool switched = container->switchTo(selectedSnapshot);
       assert(switched);
       (void) switched; // Silence compilation warning about unused variable.
